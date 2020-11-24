8fe093eecd8a02016c1944d9f345d9fbeb3ed3c9
nour <nour.kobbi@gmail.com>
2020-11-24 23:25:16 +0100

    1 ere version

---
+++ productExample/src/test/java/com/Isg/soa/productExample/ProductExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Isg.soa.productExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
